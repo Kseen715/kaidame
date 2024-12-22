e260d0a918f0e306878bfbf3e21dfeafa6c34281
kseen <korenevdn@gmail.com>
2024-12-23 01:00:46 +0300

    Add configuration files and update .gitignore for server setup

---
+++ server.bat
@@ -0,0 +1,1 @@
+java -jar .\kettinglauncher-1.5.0.jar -minecraftVersion 1.20.1 -noui -nologo -accepteula
---
+++ server.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -jar .\kettinglauncher-1.5.0.jar -minecraftVersion 1.20.1 -noui -nologo -accepteula
